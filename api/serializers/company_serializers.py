a9d4129f74da41d8ed8eaab209e2a0d3f7d0b3f4
Craig White <craigwhiteofficial@gmail.com>
2021-05-04 20:01:46 -0400

    Add CompanyPosition fields to CompanySerializer

--- api/serializers/company_serializers.py
+++ api/serializers/company_serializers.py
@@ -1,13 +1,39 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from api.models import Company
+from api.models import Company, CompanyPosition
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    created_by_position_title = serializers.CharField(write_only=True)
+    created_by_position_description = serializers.CharField(write_only=True, allow_blank=True)
+    created_by_position_start_date = serializers.DateField(write_only=True)
+
     class Meta:
         model = Company
-        fields = ['id', 'created', 'name', 'location', 'slogan', 'logo_url', 'email']
+        fields = ['id', 'created', 'name', 'location', 'slogan', 'logo_url', 'email', 'created_by_position_title',
+                  'created_by_position_description', 'created_by_position_start_date']
         read_only_fields = ['id', 'created']
+
+    @transaction.atomic()
+    def create(self, validated_date):
+        created_by_position = validated_date.pop('created_by_position_title')
+        created_by_position_description = validated_date.pop('created_by_position_description')
+        created_by_position_start_date = validated_date.pop('created_by_position_start_date')
+
+        company = super().create(validated_date)
+        CompanyPosition.objects.create(
+            user=self.context['request'].user,
+            company=company,
+            title=created_by_position,
+            description=created_by_position_description,
+            start_date=created_by_position_start_date,
+            end_date=None,
+            is_admin=True,
+            can_edit=True,
+            can_create_jobs=True,
+        )
+        return company
 
 
 class CompanyDetailSerializer(serializers.ModelSerializer):
