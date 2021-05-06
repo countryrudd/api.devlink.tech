83978214b9221ba80b2621c336e5b36d9073fe85
Craig White <craigwhiteofficial@gmail.com>
2021-05-05 22:02:11 -0400

    Update tests with new linkedin_username field

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,19 +8,19 @@ from api.models import User
 class TestUser:
     @pytest.mark.django_db(transaction=True)
     def test_create_duplicate_github_username(self):
-        User.objects.create(name='Test User', email='test@test.com', auth0_id='auth|12345', linkedin_id='12345',
+        User.objects.create(name='Test User', email='test@test.com', auth0_id='auth|12345', linkedin_username='12345',
                             github_username='test', is_developer=True, location='Charlotte, NC', avatar_url='',
                             languages=['English'], skills=['Python'])
         with pytest.raises(IntegrityError):
-            User.objects.create(name='Test User', email='test@test.com', auth0_id='auth|12345', linkedin_id='12345',
-                                github_username='test', is_developer=True, location='Charlotte, NC', avatar_url='',
-                                languages=['English'], skills=['Python'])
+            User.objects.create(name='Test User', email='test@test.com', auth0_id='auth|12345',
+                                linkedin_username='12345', github_username='test', is_developer=True,
+                                location='Charlotte, NC', avatar_url='', languages=['English'], skills=['Python'])
         assert User.objects.count() == 1
 
     @pytest.mark.django_db(transaction=True)
     def test_create_developer_without_github_username(self):
         with pytest.raises(IntegrityError):
-            User.objects.create(name='Test User', email='test@test.com', auth0_id='auth|12345', linkedin_id='12345',
-                                github_username='', is_developer=True, location='Charlotte, NC', avatar_url='',
-                                languages=['English'], skills=['Python'])
+            User.objects.create(name='Test User', email='test@test.com', auth0_id='auth|12345',
+                                linkedin_username='12345', github_username='', is_developer=True,
+                                location='Charlotte, NC', avatar_url='', languages=['English'], skills=['Python'])
         assert User.objects.count() == 0
