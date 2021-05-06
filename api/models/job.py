a907bc0f2b1d8d13f5c49cf1ddc5273c0514ee60
Craig White <craigwhiteofficial@gmail.com>
2021-05-06 03:15:22 -0400

    Make Job.cultures optional

--- api/models/job.py
+++ api/models/job.py
@@ -10,6 +10,6 @@ class Job(BaseModel):
     description = models.TextField()
     skills = ArrayField(base_field=models.TextField())
     languages = ArrayField(base_field=models.TextField())
-    cultures = ArrayField(base_field=models.TextField())
+    cultures = ArrayField(base_field=models.TextField(), default=list, blank=True)
     is_active = models.BooleanField(default=True)
     company = models.ForeignKey('Company', related_name='jobs', on_delete=models.CASCADE)
